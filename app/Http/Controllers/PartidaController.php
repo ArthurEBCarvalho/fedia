d376a30f34e5342e8229d76a083a34292d3df08f
Arthur <arthur_aebc@hotmail.com>
2018-07-10 15:38:18 -0300

    Commit final

--- app/Http/Controllers/PartidaController.php
+++ app/Http/Controllers/PartidaController.php
@@ -34,9 +34,9 @@ class PartidaController extends Controller {
 		if(is_null($rodada))
 			$rodada = 1;
 		if($request->tipo == "liga")
-			$partidas = Partida::where('temporada',$temporada)->where('rodada',$rodada)->where('campeonato','liga')->get();
+			$partidas = Partida::where('temporada',$temporada)->where('rodada',$rodada)->where('campeonato','Liga')->get();
 		else
-			$partidas = Partida::where('temporada',$temporada)->where('campeonato','copa')->get()->keyBy(function($item){return $item['ordem']."|".$item['rodada'];});
+			$partidas = Partida::where('temporada',$temporada)->where('campeonato','Copa')->get()->keyBy(function($item){return $item['ordem']."|".$item['rodada'];});
 		$indisponiveis = [];
 		foreach(DB::table('cartaos')->join('jogadors','cartaos.jogador_id','=','jogadors.id')->selectRaw('jogadors.nome,COUNT(*) as qtd')->where('temporada',$temporada)->where('campeonato',ucfirst($request->tipo))->where('cumprido',0)->where('cor',0)->groupBy('jogadors.nome','cartaos.time_id')->having(DB::raw('COUNT(*)'),'=',3)->get() as $suspenso)
 			$indisponiveis[] = $suspenso->nome;
