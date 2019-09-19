8142453be90837cde076926edc44572d98f08fe5
Felipe Souza <felipesouzacarvalho2@gmail.com>
2019-09-19 09:14:56 -0300

    #> Artilharia - Filtro por temporada

--- app/Http/Controllers/ArtilhariaController.php
+++ app/Http/Controllers/ArtilhariaController.php
@@ -24,19 +24,29 @@ class ArtilhariaController extends Controller
         $competicao = $request->input('competicao');
         $time_id    = $request->input('time');
 
-        $times_id = UserTime::where("era_id", Session::get('era')->id)
+        $era_id     = Session::get('era')->id;
+
+        $times_id = UserTime::where("era_id", $era_id)
                             ->pluck('time_id')
                             ->toArray();
 
         $times     = arrayToSelect(Time::whereIn('id', $times_id)->get()->toArray(), 'id', 'nome');
+        $times[0]  = 'Todos';
 
         $jogadores = Jogador::join('gols', 'gols.jogador_id', '=', 'jogadors.id')
+                            ->join('temporadas', function ($join) use ($era_id) {
+                                $join->on('temporadas.id', '=', 'gols.temporada_id')
+                                     ->where('temporadas.era_id', '=', $era_id);
+                            })
                             ->join('times', 'times.id', '=', 'jogadors.time_id')
                             ->select('jogadors.nome', 'jogadors.posicoes', 'jogadors.idade', 'jogadors.overall',
                                      \DB::raw('SUM(quantidade) as gols'), 'times.nome as nome_time')
                             ->whereIn('jogadors.time_id', $times_id)
                             ->groupBy('jogadors.id', 'times.nome')
                             ->orderBy('gols', 'DESC');
+
+        if($temporada)
+		    $jogadores->where('temporadas.numero', $temporada);
 
         if($competicao)
             $jogadores->where('gols.campeonato', $competicao);
